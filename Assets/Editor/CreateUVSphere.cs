9dcfa1de9fffbaa0c7082522c723a21fbf29b6ee
Lane Kolbly <lane@rscheme.org>
2018-02-12 21:34:26 -0600

    Fixed UV wrapping of planets by generating own sphere. Also moved planet generator into DesertGenerator subclass.

---
+++ Assets/Editor/CreateUVSphere.cs
@@ -0,0 +1,129 @@
+﻿// Based on http://wiki.unity3d.com/index.php/CreateIcoSphere
+
+using UnityEngine;
+using UnityEditor;
+using System.Collections;
+using System.Collections.Generic;
+
+public class CreateUVSphere : ScriptableWizard
+{
+
+    public int numberOfSlices = 10;
+    public int numberOfRings = 10;
+    public float radius = 0.5f;
+    public bool addCollider = false;
+    public string optionalName;
+
+    static Camera cam;
+    static Camera lastUsedCam;
+
+    [MenuItem("GameObject/Create Other/UvSphere...")]
+    static void CreateWizard()
+    {
+        cam = Camera.current;
+        // Hack because camera.current doesn't return editor camera if scene view doesn't have focus
+        if (!cam)
+            cam = lastUsedCam;
+        else
+            lastUsedCam = cam;
+        ScriptableWizard.DisplayWizard("Create UvSphere", typeof(CreateUVSphere));
+    }
+
+
+    void OnWizardUpdate()
+    {
+        if (numberOfSlices < 3) numberOfSlices = 3;
+        if (numberOfRings < 4) numberOfRings = 4;
+    }
+
+    void OnWizardCreate()
+    {
+        GameObject sphere = new GameObject();
+
+        if (!string.IsNullOrEmpty(optionalName))
+            sphere.name = optionalName;
+        else
+            sphere.name = "UvSphere";
+
+        sphere.transform.position = Vector3.zero;
+
+        MeshFilter filter = (MeshFilter)sphere.AddComponent(typeof(MeshFilter));
+        sphere.AddComponent(typeof(MeshRenderer));
+
+        string sphereAssetName = sphere.name + numberOfRings + "_" + numberOfSlices + ".asset";
+        Mesh mesh = (Mesh)AssetDatabase.LoadAssetAtPath("Assets/Editor/" + sphereAssetName, typeof(Mesh));
+
+        if (mesh == null)
+        {
+            mesh = new Mesh();
+            mesh.name = sphere.name;
+
+            List<Vector3> vertList = new List<Vector3>();
+            List<Vector2> UvList = new List<Vector2>();
+
+            // Note: Topologically, this UV sphere is a grid
+
+            List<int> triList = new List<int>();
+            for (var ring = 0; ring < numberOfRings; ++ring)
+            {
+                for (var slice = 0; slice < numberOfSlices; ++slice)
+                {
+                    var xzRadius = Mathf.Sin((float)ring / (numberOfRings - 1) * 3.14159265f);
+                    vertList.Add(new Vector3(
+                        xzRadius * Mathf.Sin((float)slice / (numberOfSlices - 1) * 2 * 3.14159265f), // Minus 1 because we want the last vertices to overlap with the first
+                        radius * 2 * Mathf.Cos((float)ring / (numberOfRings - 1) * 3.14159265f),
+                        xzRadius * Mathf.Cos((float)slice / (numberOfSlices - 1) * 2 * 3.14159265f)
+                    ));
+                    UvList.Add(new Vector2(
+                        (float)slice / numberOfSlices,
+                        (float)ring / numberOfRings
+                    ));
+                }
+
+                if (ring > 0)
+                {
+                    // Connect this ring to the last one
+                    for (var slice = 0; slice < numberOfSlices - 1; ++slice)
+                    {
+                        var vertexOffset = (ring - 1) * numberOfSlices + slice;
+
+                        // Face 1
+                        triList.Add(1 + vertexOffset);
+                        triList.Add(0 + vertexOffset);
+                        triList.Add(numberOfSlices + vertexOffset);
+                        //faces.Add(new TriangleIndices(1 + vertexOffset, 0 + vertexOffset, numberOfSlices + vertexOffset));
+
+                        // Face 2
+                        triList.Add(1 + vertexOffset);
+                        triList.Add(numberOfSlices + vertexOffset);
+                        triList.Add(numberOfSlices + 1 + vertexOffset);
+                        //faces.Add(new TriangleIndices(1 + vertexOffset, numberOfSlices + vertexOffset, numberOfSlices + 1 + vertexOffset));
+                    }
+                }
+            }
+
+            mesh.vertices = vertList.ToArray();
+            mesh.triangles = triList.ToArray();
+            mesh.uv = UvList.ToArray();
+
+            Vector3[] normals = new Vector3[vertList.Count];
+            for (int i = 0; i < normals.Length; i++)
+                normals[i] = vertList[i].normalized;
+
+            mesh.normals = normals;
+
+            mesh.RecalculateBounds();
+
+            AssetDatabase.CreateAsset(mesh, "Assets/Editor/" + sphereAssetName);
+            AssetDatabase.SaveAssets();
+        }
+
+        filter.sharedMesh = mesh;
+        mesh.RecalculateBounds();
+
+        if (addCollider)
+            sphere.AddComponent(typeof(SphereCollider));
+
+        Selection.activeObject = sphere;
+    }
+}
---
+++ Assets/Scripts/PlanetGenerators/DesertGenerator.cs
@@ -0,0 +1,78 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DesertGenerator : GeneratePlanet {
+
+    public INoiseGenerator noiseSource;
+    public AnimationCurve colorCurve;
+
+    private Color ColorScale(float value, List<Color> palette)
+    {
+        float scaleValue = value * (palette.Count - 1);
+        int idx = Mathf.FloorToInt(scaleValue);
+        float remainder = scaleValue - idx;
+
+        if (idx >= palette.Count - 1) return palette[palette.Count - 1];
+        if (idx < 0) return palette[0];
+
+        Color color1 = palette[idx];
+        Color color2 = palette[idx + 1];
+        return Color.Lerp(color1, color2, remainder);
+    }
+
+    public override void Generate()
+    {
+        var startTime = Time.realtimeSinceStartup;
+
+        int width = resolution;
+        int height = resolution;
+        var color = new Texture2D(width, height, TextureFormat.ARGB32, false);
+        color.filterMode = FilterMode.Point;
+        var albedo = new Texture2D(width, height, TextureFormat.ARGB32, false);
+        albedo.filterMode = FilterMode.Point;
+
+        var heightMap = new float[width, height];
+
+        var colors = new Color[width * height];
+        var albedoLevels = new Color[width * height];
+
+        var scale = 9.0f;
+
+        for (int x = 0; x < width; ++x)
+        {
+            for (int y = 0; y < height; ++y)
+            {
+                var radius = Mathf.Cos(((float)y / height - 0.5f) * 3.14159265f);// * scale;
+                var pixelSpaceCoords = new Vector3(
+                    Mathf.Sin((float)x / width * 3.14159265f * 2.0f) * radius,
+                    Mathf.Sin(((float)y / height - 0.5f) * 3.14159265f),
+                    Mathf.Cos((float)x / width * 3.14159265f * 2.0f) * radius
+                );
+
+                var noise = noiseSource.ValueAtPoint(pixelSpaceCoords);
+                colors[x + width * y] = ColorScale(colorCurve.Evaluate(noise), new List<Color> {
+                    new Color(229.0f/255.0f, 172.0f/255.0f, 114.0f/255.0f),
+                    new Color(0.5f, 0.4f, 0.0f),
+                    new Color(1.0f, 1.0f, 1.0f)
+                });
+                colors[x + width * y].a = 0.9f;
+
+                heightMap[x, y] = noise;
+            }
+        }
+
+        color.SetPixels(colors);
+        albedo.SetPixels(albedoLevels);
+
+        color.Apply();
+        albedo.Apply();
+
+        material.EnableKeyword("_SPECGLOSSMAP");
+        material.SetTexture("_MainTex", color);
+        material.SetTexture("_SpecGlossMap", albedo);
+
+        var totalTime = Time.realtimeSinceStartup - startTime;
+        Debug.Log("Generating planet took " + totalTime + "s");
+    }
+}
