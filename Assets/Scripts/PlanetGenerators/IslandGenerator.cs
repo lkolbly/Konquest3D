822497f8b4e78c298505a7a6fe90390f5180463a
Lane Kolbly <lane@rscheme.org>
2018-02-12 21:46:11 -0600

    Created IslandGenerator

--- Assets/Scripts/PlanetGenerators/IslandGenerator.cs
+++ Assets/Scripts/PlanetGenerators/IslandGenerator.cs
@@ -2,24 +2,10 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class DesertGenerator : GeneratePlanet {
+public class IslandGenerator : GeneratePlanet
+{
 
     public INoiseGenerator noiseSource;
-    public AnimationCurve colorCurve;
-
-    private Color ColorScale(float value, List<Color> palette)
-    {
-        float scaleValue = value * (palette.Count - 1);
-        int idx = Mathf.FloorToInt(scaleValue);
-        float remainder = scaleValue - idx;
-
-        if (idx >= palette.Count - 1) return palette[palette.Count - 1];
-        if (idx < 0) return palette[0];
-
-        Color color1 = palette[idx];
-        Color color2 = palette[idx + 1];
-        return Color.Lerp(color1, color2, remainder);
-    }
 
     public override void Generate()
     {
@@ -29,10 +15,13 @@ public class DesertGenerator : GeneratePlanet {
         int height = resolution;
         var color = new Texture2D(width, height, TextureFormat.ARGB32, false);
         color.filterMode = FilterMode.Point;
+        var albedo = new Texture2D(width, height, TextureFormat.ARGB32, false);
+        albedo.filterMode = FilterMode.Point;
 
         var heightMap = new float[width, height];
 
         var colors = new Color[width * height];
+        var albedoLevels = new Color[width * height];
 
         for (int x = 0; x < width; ++x)
         {
@@ -46,20 +35,39 @@ public class DesertGenerator : GeneratePlanet {
                 );
 
                 var noise = noiseSource.ValueAtPoint(pixelSpaceCoords);
-                colors[x + width * y] = ColorScale(colorCurve.Evaluate(noise), new List<Color> {
-                    new Color(229.0f/255.0f, 172.0f/255.0f, 114.0f/255.0f),
-                    new Color(0.5f, 0.4f, 0.0f),
-                    new Color(1.0f, 1.0f, 1.0f)
-                });
-                colors[x + width * y].a = 0.9f;
+                var idx = x + width * y;
+                if (noise < 0.75f)
+                {
+                    colors[idx] = Color.green;
+                    //colors[x + width * y].a = 0.4f;
+                    albedoLevels[idx].r = 0.0f;
+                    albedoLevels[idx].g = 0.0f;
+                    albedoLevels[idx].b = 1.0f;
+                    albedoLevels[idx].a = 0.9f;
+                }
+                else
+                {
+                    colors[idx] = Color.blue;
+                    //colors[x + width * y].a = 0.9f;
+                    albedoLevels[idx].r = 0.0f;
+                    albedoLevels[idx].g = 1.0f;
+                    albedoLevels[idx].b = 0.0f;
+                    albedoLevels[idx].a = 0.2f;
+                }
 
                 heightMap[x, y] = noise;
             }
         }
 
         color.SetPixels(colors);
+        albedo.SetPixels(albedoLevels);
+
         color.Apply();
+        albedo.Apply();
+
+        material.EnableKeyword("_SPECGLOSSMAP");
         material.SetTexture("_MainTex", color);
+        material.SetTexture("_SpecGlossMap", albedo);
 
         var totalTime = Time.realtimeSinceStartup - startTime;
         Debug.Log("Generating planet took " + totalTime + "s");
