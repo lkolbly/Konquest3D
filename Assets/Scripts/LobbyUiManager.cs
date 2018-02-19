520beb500f6e5fe40088532cf80e4341bb9bf205
Lane Kolbly <lane@rscheme.org>
2018-02-18 18:03:08 -0600

    Started setting up in-VR network lobby UI

---
+++ Assets/Scripts/LobbyUiManager.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LobbyUiManager : MonoBehaviour {
+
+    public Canvas mainCanvas;
+    public Canvas matchCanvas;
+    public MyNetworkManager networkManager;
+
+	// Use this for initialization
+	void Start () {
+        matchCanvas.gameObject.SetActive(false);
+	}
+
+    public void OnStartServerClicked()
+    {
+        networkManager.StartServer();
+
+        SwitchToMatchLobbyUi();
+    }
+
+    public void OnConnectToServerClicked()
+    {
+        networkManager.networkAddress = "pillow.rscheme.org";
+        networkManager.networkPort = 3058;
+        networkManager.StartClient();
+
+        SwitchToMatchLobbyUi();
+    }
+
+    public void OnReadyClicked()
+    {
+        //
+    }
+
+    public void OnLeaveMatchClicked()
+    {
+        //
+    }
+
+    private void SwitchToMatchLobbyUi()
+    {
+        mainCanvas.gameObject.SetActive(false);
+        matchCanvas.gameObject.SetActive(true);
+    }
+
+    // Update is called once per frame
+    void Update () {
+		
+	}
+}
